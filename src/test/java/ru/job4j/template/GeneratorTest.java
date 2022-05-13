36a55a4643a5a5c6533aaf994ee223f8852f4e43
aslanmagamaev <aslanmagamaev@mail.ru>
2022-05-13 17:31:54 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class GeneratorTest {
+
+    @Test
+    public void whenWorks() {
+        Generator generator = new GeneratorFirst();
+        String temp = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Aslan");
+        map.put("subject", "you");
+        String rsl = generator.produce(temp, map);
+        assertThat(rsl, is("I am a Aslan, Who are you?"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNoKey() {
+        Generator generator = new GeneratorFirst();
+        String temp = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Aslan");
+        String rsl = generator.produce(temp, map);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenExtraKey() {
+        Generator generator = new GeneratorFirst();
+        String temp = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Aslan");
+        map.put("surname", "Magamaev");
+        String rsl = generator.produce(temp, map);
+    }
+}
