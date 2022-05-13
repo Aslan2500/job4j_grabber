b4669990c06dc94648eb27405437db3bbebf0af1
aslanmagamaev <aslanmagamaev@mail.ru>
2022-05-13 13:14:31 +0300

    1. Принципы Kiss, Dry и Yagni [#238813 #300330]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -1,31 +1,27 @@
 package ru.job4j.kiss;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiPredicate;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        return compare(value, comparator).get(1);
+        BiPredicate<T, T> biPredicate = (o1, o2) -> comparator.compare(o1, o2) > 0;
+        return compare(value, biPredicate);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        return compare(value, comparator).get(0);
+        BiPredicate<T, T> biPredicate = (o1, o2) -> comparator.compare(o1, o2) < 0;
+        return compare(value, biPredicate);
     }
 
-    public static <T> List<T> compare(List<T> value, Comparator<T> comparator) {
-        T min = value.get(0);
-        T max = value.get(0);
+    public static <T> T compare(List<T> value, BiPredicate<T, T> biPredicate) {
+        T rsl = value.get(0);
         for (T val : value) {
-            if (comparator.compare(min, val) > 0) {
-                min = val;
-            } else if (comparator.compare(max, val) < 0) {
-                max = val;
+            if (biPredicate.test(val, rsl)) {
+                rsl = val;
             }
         }
-        List<T> rsl = new ArrayList<>();
-        rsl.add(min);
-        rsl.add(max);
         return rsl;
     }
 }
